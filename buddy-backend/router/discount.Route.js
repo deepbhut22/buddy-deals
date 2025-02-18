cf574c5f6352071616dab8244cb75597f22d1fd9
deepbhut22 <deeppatel6071@gmail.com>
2025-02-18 23:08:07 +0530

    temp changes

--- buddy-backend/router/discount.Route.js
+++ buddy-backend/router/discount.Route.js
@@ -8,26 +8,26 @@ const discountRouter = express.Router();
 // Route to get all discounts, coupons, and URL discounts
 discountRouter.get("/products", getAllProducts);
 
-discountRouter.use((req, res, next) => {
-    try {                
-        const token = req.cookies.buddyToken;
+// discountRouter.use((req, res, next) => {
+//     try {                
+//         const token = req.cookies.buddyToken;
 
-        if(!token) {            
-            return res.status(401).json({message: "You are not authenticated!"});
-        }
+//         if(!token) {            
+//             return res.status(401).json({message: "You are not authenticated!"});
+//         }
 
-        const decoded = jwt.verify(token, process.env.SECRET);
-        // console.log(decoded);
+//         const decoded = jwt.verify(token, process.env.SECRET);
+//         // console.log(decoded);
 
-        req.userId = decoded.id;
-        req.userEmail = decoded.email;
+//         req.userId = decoded.id;
+//         req.userEmail = decoded.email;
         
-        next();
+//         next();
         
-    } catch (error) {
-        return res.status(401).json({message: "Invalid Token!"});
-    }
-});
+//     } catch (error) {
+//         return res.status(401).json({message: "Invalid Token!"});
+//     }
+// });
 
 // Route to get coupon code by ID
 discountRouter.get("/products/discount/:id", getDiscountCodeByIdController);
